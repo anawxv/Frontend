442069590b367a01f5855f660e05a29eb1432a7e
Rafael Vasconcelos <vasconceloss.raffa@gmail.com>
2023-05-04 22:00:05 -0300

    Atualizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ListarProdutosComponent } from './components/produtos/listar-produtos/listar-produtos.component';
 import { HomeComponent } from './components/home/home.component';
+import { ListarProdutosComponent } from './components/produtos/listar-produtos/listar-produtos.component';
 import { CadastrarProdutosComponent } from './components/produtos/cadastrar-produtos/cadastrar-produtos.component';
 
 const routes: Routes = [
-  {path:'', component:HomeComponent},
-  {path:'produtos', component:ListarProdutosComponent},
-  {path:'produtos/cadastrar', component:CadastrarProdutosComponent}
+  { path: '', component:HomeComponent },
+  { path: 'produtos', component:ListarProdutosComponent},
+  { path: 'produtos/cadastrar', component: CadastrarProdutosComponent}
 ];
 
 @NgModule({
