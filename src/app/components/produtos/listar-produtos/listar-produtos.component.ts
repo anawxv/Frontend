c29ce507ca19a0251325ebe7ce76d3c84116662a
Ana Livia <analirajonhson@gmail.com>
2023-05-11 22:06:55 -0300

    arrays, objetos, for e *ngFor no Angular

--- src/app/components/produtos/listar-produtos/listar-produtos.component.ts
+++ src/app/components/produtos/listar-produtos/listar-produtos.component.ts
@@ -5,6 +5,34 @@ import { Component } from '@angular/core';
   templateUrl: './listar-produtos.component.html',
   styleUrls: ['./listar-produtos.component.css']
 })
-export class ListarProdutosComponent {
+
+export class ListarProdutosComponent  {
+  listaStrings: string[] = ['Primeiro', 'Segundo', 'Terceiro'];
+  listaNumeros: number[] = [15, 15.18, 100];
+
+  objetoModelo = {
+    nome: 'Ana',
+    idade: 18,
+    altura: 1.54,
+    graduado: true
+  };
+
+  listaProdutos: any[] = [
+    {nome: 'Curso de Angular', precoProduto: 35.56, validade: '2021-12-31', id: 1},
+    {nome: 'Curso de Ionic', precoProduto: 50, validade: '2021-12-31', id: 2, promocao: true},
+    {id:3, nome: 'Curso de Ionic Avançado', precoProduto:50, validade: '2021-12-31'},
+  ];
+
+  constructor() {
+    for(let item of this.listaStrings) {
+      console.log(item);
+    }
+    for(const item of this.listaNumeros) {
+      console.log(item);
+    }
+
+    console.log(this.objetoModelo);
+    console.log(this.objetoModelo.nome);
+  }
 
 }
